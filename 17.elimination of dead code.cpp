1af5a4b437f65ae0eac522625f9086f543ebfbe1
prathapreddybattula <112234878+prathapreddybattula@users.noreply.github.com>
2023-11-16 10:33:56 +0530

    Create 17.elimination of dead code.cpp

---
+++ 17.elimination of dead code.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int flag = 0; 
+
+    if (flag == 1) {
+        printf("Flag is set.\n");
+    } else {
+        printf("Flag is not set.\n");
+    }
+
+    return 0;
+}
